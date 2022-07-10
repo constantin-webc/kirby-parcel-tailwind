b5cbbe1ea0cabbbf4636a8e67be903cba0681653
constantin <constantin@fastmail.com>
2022-07-10 18:56:43 +0200

    remove unused script

--- src/main.js
+++ src/main.js
@@ -1,9 +1,3 @@
-// Test to force full reload on php file change
-// if (module.hot) {
-//   module.hot.dispose(function () {
-//     window.location.reload();
-//   });
-// }
 import showSomething from './js/components/test';
 showSomething();
 
